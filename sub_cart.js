ff877c96c0604c7085ee3f51c28cacee49214b56
Unknown <CSnow040@west-mec.org>
2019-05-09 16:21:32 -0700

    Final Commit
    Finished Project that I forgot about due to absence, ready to be fully graded

--- sub_cart.js
+++ sub_cart.js
@@ -23,15 +23,34 @@
 */
 
 window.addEventListener("load", setupCart);
-
+// Creates a function that calls in the aside element with the class of addButton. Every time the addButton is clicked, an item is added
 function setupCart() {
-      var addButtons = document.getElementsByClassName("addButton");
+      var addButtons = document.getElementsByClassName("addButton")
       for (var i = 0; i < addButtons.length; i++) {
-            addButtons[i].onclick = addItem;
+            addButtons[i].onclick = addItem
       }
 }
-
+//Creates a function that adds an item to the cart and allows for it to be added many times by cloning the node
 function addItem(e) {
-      var foodItem = e.target.getElementSibiling;
-      var foodID = foodItem.setAttribute("id");
+      var foodItem = e.target.nextElementSibling;
+      var foodID = foodItem.getAttribute("id");
+      var foodDescription = foodItem.cloneNode(true);
+      var cartBox = document.getElementById("cart")
+      var duplicateOrder = false;
+      for (var n = cartBox.firstChild; n = n.nextElementSibling; n !== null) {
+            if (n.id === foodID) {
+                  duplicateOrder = true;
+                  n.firstChild.textContent++;
+                  break;
+            }
+
+      }
+      // If the duplicate order variable is returns a value of false, it will store the span element in the orderCount variable and will set the orderCount to the text string of 1
+      if (duplicateOrder === false) {
+            var orderCount = document.createElement("span")
+            orderCount.textContent = "1";
+            foodDescription.insertBefore(orderCount, foodDescription.firstChild);
+            cartBox.appendChild(foodDescription);
+
+      }
 }
